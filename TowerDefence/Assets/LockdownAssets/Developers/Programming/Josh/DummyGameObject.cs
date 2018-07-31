7d878fa30e0cdba1989d5fde9dcb270d62187a7f
AIE\s170998 <eljosho1998@gmail.com>
2018-07-31 16:02:54 +1000

    Some of my test scene stuff.

---
+++ TowerDefence/Assets/LockdownAssets/Developers/Programming/Josh/DummyGameObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyGameObject : WorldObject {
+
+    public GameObject other;
+
+    public Rigidbody rb;
+
+    public WorldObject wo;
+
+    public int speed = 500;
+
+	protected override void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+        wo.SetHitPoints(100);
+
+    }
+	
+	protected override void Update () {
+        base.Update();
+
+        // Move upwards
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+    }
+}
