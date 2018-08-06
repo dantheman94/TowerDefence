ce43b5b907d3cc7635247ca6e7053370916a123f
Axel245 <angus-rox@live.com.au>
2018-08-06 14:18:36 +1000

    Finished Highscore system. Created new Panel class for storing panel data. Sorted list by score now working.

--- TowerDefence/Assets/LockdownAssets/Scripts/UI/MainMenu/Display_Leaderboard.cs
+++ TowerDefence/Assets/LockdownAssets/Scripts/UI/MainMenu/Display_Leaderboard.cs
@@ -2,39 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Linq;
 
 //******************************
 //
 //  Created by: Angus Secomb
 //
 //  Last edited by: Angus Secomb
-//  Last edited on: 4/08/2018
+//  Last edited on: 6/08/2018
 //
 //******************************
 
 
 public class Display_Leaderboard : MonoBehaviour {
 
-    public struct Panel
-    {
-        public Text Rank;
-        public Text Name;
-        public Text Waves;
-        public Text Score;
-        public Text Difficulty;
-        public Text Win;
-        public Image Backdrop;
-        public Color ColorBackdrop;
-        public Color TextColor;
-        public Transform PanelPosition;
-    }
     public struct MapPanel
     {
         public Image MapImage;
         public Text MapName;
         public Transform PanelPosition;
     }
-
+    public GameObject PanelObject;
+    public GameObject ListObject;
 
     //******************************************************************************************************************************
     //
@@ -52,7 +43,6 @@ public class Display_Leaderboard : MonoBehaviour {
     //******************************************************************************************************************************
 
     //Variables
-    private List<Panel> _HighscorePanels;
     private List<MapPanel> _MapPanels;
     private List<Leaderboard.SaveData> _SaveDataList;
     private List<Leaderboard.SaveData> _SortedSaveData;
@@ -65,37 +55,49 @@ public class Display_Leaderboard : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+       _SaveDataList = Leaderboard.Instance.LoadData();
+        SortHighscores();
+
+        InstantiatePanels();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    /// <summary>
-    /// Retrieve all saved highscore data.
-    /// </summary>
-   public void RetrieveHighscores()
-    {
-
-    }
-
 
     /// <summary>
     /// Sorts high score data.
     /// </summary>
-    public void SortHighScores()
+    public void SortHighscores()
     {
-
+        _SortedSaveData = _SaveDataList.OrderByDescending(o => o.Score).ToList();
     }
 
-
     /// <summary>
-    /// Load High scores to gui.
+    /// Instantiates each panel for the highscore list.
     /// </summary>
-    public void LoadHighScores()
+    public void InstantiatePanels()
     {
-
+        int counterIncrement = 18;
+        for(int i =0; i < _SortedSaveData.Count;++i)
+        {
+   
+            {
+                GameObject go = Instantiate(PanelObject);
+                go.SetActive(true);
+                go.transform.parent = ListObject.transform;
+                go.transform.localPosition = PanelObject.transform.localPosition;
+                if(i != 0)
+                {
+                    go.transform.localPosition = new Vector3(go.transform.localPosition.x, go.transform.localPosition.y - counterIncrement);
+                }
+           
+               
+                Panel panel = go.GetComponent<Panel>();
+                panel.Score.text = _SortedSaveData[i].Score.ToString();
+                panel.Difficulty.text = _SortedSaveData[i].Difficulty;
+                panel.Name.text = _SortedSaveData[i].Name;
+                panel.Win.text = _SortedSaveData[i].Outcome;
+                panel.Waves.text = _SortedSaveData[i].Waves.ToString();
+                panel.Rank.text = (i + 1).ToString();
+                counterIncrement += 18;
+            }
+        }
     }
 }
---
+++ TowerDefence/Assets/LockdownAssets/Scripts/UI/MainMenu/Panel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//******************************
+//
+//  Created by: Angus Secomb
+//
+//  Last edited by: Angus Secomb
+//  Last edited on: 6/08/2018
+//
+//******************************
+
+
+public class Panel : MonoBehaviour {
+
+    //Public variables
+
+    public Text Rank;
+    public Text Name;
+    public Text Waves;
+    public Text Score;
+    public Text Difficulty;
+    public Text Win;
+    public Image Backdrop;
+    public Color ColorBackdrop;
+    public Color TextColor;
+
+}
