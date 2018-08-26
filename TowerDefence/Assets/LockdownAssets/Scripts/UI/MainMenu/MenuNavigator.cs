3f1a8935a064ab3bc77bd31cf6ddc55a85233e36
Angus <angycs123@hotmail.com>
2018-08-27 09:40:03 +1000

    Added new menu scripts. Includes controller support. Still a few small bugs to fix.

---
+++ TowerDefence/Assets/LockdownAssets/Scripts/UI/MainMenu/MenuNavigator.cs
@@ -0,0 +1,296 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
+
+public class MenuNavigator : MonoBehaviour {
+
+    public enum SceneAreaState
+    {
+        ACTIVE,
+        INACTIVE
+    }
+
+    [System.Serializable]
+    public struct MenuArea
+    {
+        public SceneAreaState AreaState;
+        [Tooltip("Reference to menu area wrapper.")]
+        public GameObject WholeAreaObject;
+        [Tooltip("The button to return to the previous menu area.")]
+        public Button PreviousButton;
+        [Tooltip("The button where selection starts after entering a new area.")]
+        public Button StartButton;
+        public Dropdown DropButton;
+    }
+
+
+
+    [Tooltip("Main menu area.")]
+    public MenuArea MainMenu;
+    [Tooltip("Play Menu Area")]
+    public MenuArea PlayMenu;
+    [Tooltip("Settings Menu Area")]
+    public MenuArea SettingsMenu;
+    [Tooltip("Leaderboard Menu Area")]
+    public MenuArea LeaderboardMenu;
+    [Tooltip("Credit Menu Area")]
+    public MenuArea CreditsMenu;
+
+    public GameObject LevelUIObject;
+
+    public GameObject DifficultyUIObject;
+
+    public Button LevelStartButton;
+    public Button DifficultyStartButton;
+
+    public List<Image> ButtonImage;
+
+
+
+    private MenuArea _ActiveMenu;
+
+    private xb_gamepad gamepad;
+
+	// Use this for initialization
+	void Start () {
+        gamepad = GamepadManager.Instance.GetGamepad(1);
+
+        MainMenu.AreaState = SceneAreaState.ACTIVE;
+        PlayMenu.AreaState = SceneAreaState.INACTIVE;
+        LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+        SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+        CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+        MainMenu.StartButton.Select();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavigateMenu();
+        ChangeButtonColor();
+	}
+
+
+    /// <summary>
+    /// Enter desired menu area.
+    /// </summary>
+    /// <param name="DesiredMenu"></param>
+    public void EnterMenuArea(string DesiredMenu)
+    {
+        switch(DesiredMenu)
+        {
+            case "Main":
+                MainMenu.AreaState = SceneAreaState.ACTIVE;
+                SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+                LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+                PlayMenu.AreaState = SceneAreaState.INACTIVE;
+                CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+                if (MainMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(MainMenu.StartButton));
+                
+                break;
+            case "Settings":
+                SettingsMenu.AreaState = SceneAreaState.ACTIVE;
+                MainMenu.AreaState = SceneAreaState.INACTIVE;
+                LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+                PlayMenu.AreaState = SceneAreaState.INACTIVE;
+                CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+                if (SettingsMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(SettingsMenu.StartButton));
+        
+                break;
+            case "Leaderboard":
+                LeaderboardMenu.AreaState = SceneAreaState.ACTIVE;
+                SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+                PlayMenu.AreaState = SceneAreaState.INACTIVE;
+                MainMenu.AreaState = SceneAreaState.INACTIVE;
+                CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+                if (LeaderboardMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(LeaderboardMenu.StartButton));
+                break;
+            case "Play":
+                PlayMenu.AreaState = SceneAreaState.ACTIVE;
+                MainMenu.AreaState = SceneAreaState.INACTIVE;
+                SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+                LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+                CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+                if (PlayMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(PlayMenu.StartButton));
+                break;
+            case "Credits":
+                CreditsMenu.AreaState = SceneAreaState.ACTIVE;
+                MainMenu.AreaState = SceneAreaState.INACTIVE;
+                SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+                LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+                PlayMenu.AreaState = SceneAreaState.INACTIVE;
+                if (CreditsMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(CreditsMenu.StartButton));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void EnterPlayArea(string DesiredSubMenu)
+    {
+        switch (DesiredSubMenu)
+        {
+            case "Level":
+                LevelUIObject.SetActive(true);
+                DifficultyUIObject.SetActive(false);
+                StartCoroutine(DelayedSelectButton(LevelStartButton));
+                break;
+            case "Difficulty":
+                LevelUIObject.SetActive(false);
+                DifficultyUIObject.SetActive(true);
+                StartCoroutine(DelayedSelectButton(DifficultyStartButton));
+                break;
+            case "Start Match":
+
+                break;
+        }
+    
+    }
+
+    /// <summary>
+    /// Switches between menu states.
+    /// </summary>
+    private void NavigateMenu()
+    {
+        if(MainMenu.AreaState == SceneAreaState.ACTIVE)
+        {
+            MainMenu.WholeAreaObject.SetActive(true);
+            
+        
+        }
+        else
+        {
+            MainMenu.WholeAreaObject.SetActive(false);
+        }
+
+        if(SettingsMenu.AreaState == SceneAreaState.ACTIVE)
+        {
+            SettingsMenu.WholeAreaObject.SetActive(true);
+            if(gamepad.GetButtonDown("B"))
+            {
+                SettingsMenu.AreaState = SceneAreaState.INACTIVE;
+                MainMenu.AreaState = SceneAreaState.ACTIVE;
+                if (MainMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(MainMenu.StartButton));
+            }
+
+        }
+        else
+        {
+            SettingsMenu.WholeAreaObject.SetActive(false);
+        }
+
+        if(LeaderboardMenu.AreaState == SceneAreaState.ACTIVE)
+        {
+            LeaderboardMenu.WholeAreaObject.SetActive(true);
+            if(gamepad.GetButtonDown("B"))
+            {
+                LeaderboardMenu.AreaState = SceneAreaState.INACTIVE;
+                MainMenu.AreaState = SceneAreaState.ACTIVE;
+                if (MainMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(MainMenu.StartButton));
+            }
+
+        }
+        else
+        {
+            LeaderboardMenu.WholeAreaObject.SetActive(false);
+        }
+
+        if(PlayMenu.AreaState == SceneAreaState.ACTIVE)
+        {
+            PlayMenu.WholeAreaObject.SetActive(true);
+            if(gamepad.GetButtonDown("B"))
+            {
+                PlayMenu.AreaState = SceneAreaState.INACTIVE;
+                MainMenu.AreaState = SceneAreaState.ACTIVE;
+                if (MainMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(MainMenu.StartButton));
+            }
+
+        }
+        else
+        {
+            PlayMenu.WholeAreaObject.SetActive(false);
+        }
+
+        if(CreditsMenu.AreaState == SceneAreaState.ACTIVE)
+        {
+            CreditsMenu.WholeAreaObject.SetActive(true);
+            if(gamepad.GetButtonDown("B"))
+            {
+                MainMenu.AreaState = SceneAreaState.ACTIVE;
+                CreditsMenu.AreaState = SceneAreaState.INACTIVE;
+                if (MainMenu.StartButton != null)
+                    StartCoroutine(DelayedSelectButton(MainMenu.StartButton));
+            }
+
+        }
+        else
+        {
+            CreditsMenu.WholeAreaObject.SetActive(false);
+        }   
+    }
+
+    /// <summary>
+    /// Selects button after 0.05 seconds as unity wont register
+    /// the highlight without it.
+    /// </summary>
+    /// <param name="a_button"></param>
+    /// <returns></returns>
+    IEnumerator DelayedSelectButton(Button a_button)
+    {
+        yield return new WaitForSeconds(0.05f);
+        a_button.Select();
+    }
+
+    IEnumerator DelayedSelectDropdown(Dropdown dropdown)
+    {
+        yield return new WaitForSeconds(0.05f);
+        dropdown.Select();
+    }
+
+
+    /// <summary>
+    /// Presses "B" to return to the previous screen.
+    /// </summary>
+    private void GoBack()
+    {
+        if(gamepad.GetButtonDown("B"))
+        {
+
+        }
+    }
+
+    private void ChangeButtonColor()
+    {
+        if(gamepad.GetButton("B"))
+        {
+            for(int i = 0; i < ButtonImage.Count; i++)
+            {
+                ButtonImage[i].color = Color.grey;  
+            }
+        }
+        else if(!gamepad.GetButton("B"))
+        {
+            for (int i = 0; i < ButtonImage.Count; i++)
+            {
+                ButtonImage[i].color = Color.white;
+            }
+        }
+    }
+
+    public void Test()
+    {
+        Debug.Log("test!!!");
+    }
+
+   
+}
---
+++ TowerDefence/Assets/LockdownAssets/Scripts/UI/MainMenu/SettingsMenuNavigator.cs
@@ -0,0 +1,384 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenuNavigator : MonoBehaviour {
+
+    [System.Serializable]
+    public struct SettingsInfo
+    {
+        public Button SelectionMenuTypeNav;
+        public Button WindowModeNav;
+        public Button ResolutionNav;
+        public Button AspectNav;
+        public Button VSyncNav;
+        public Button TextureQualityNav;
+        public Button MasterVolumeNav;
+        public Button MusicVolumeNav;
+        public Button SFXVolumeNav;
+        public Button SelectionMenuLeft;
+        public Button SelectionMenuRight;
+        public Button GamePadSchemes;
+        public Button KeyboardBindings;
+        public Button WindowModeLeft;
+        public Button WindowModeRight;
+        public Button ResolutionLeft;
+        public Button ResolutionRight;
+        public Button AspectRatioLeft;
+        public Button AspectRatioRight;
+        public Button V_SyncLeft;
+        public Button V_SyncRight;
+        public Button TextureQualityLeft;
+        public Button TextureQualityRight;
+    }
+
+    public enum MenuType
+    {
+        RADIAL, WINDOW
+    }
+
+    public enum TextureQuality
+    {
+        LOW, MEDIUM, HIGH, ULTRA
+    }
+
+    public enum WindowMode
+    {
+        FULLSCREEN, WINDOWED
+    }
+
+    public enum Resolution
+    {
+        NINETEEN_TWENTY_BY_TEN_EIGHTY,
+    }
+
+    public enum AspectRatio
+    {
+        SIXTEEN_NINE,
+    }
+    
+    [Tooltip("0 = false 1 = true for vsync")]
+    public int VSync = 0;
+
+    private AspectRatio _AspectRatio;
+    private Resolution _Resolution;
+    private WindowMode _WindowMode;
+    private TextureQuality _TextureQuality;
+    private MenuType _MenuType;
+
+    public string CurrentSelection;
+
+    public float MasterVolume = 100;
+    public float MusicVolume = 100;
+    public float EffectsVolume = 100;
+
+
+    public Text AspectRatioText;
+    public Text MenuTypeText;
+    public Text TextureQualityText;
+    public Text VSyncText;
+    public Text ResolutionText;
+    public Text WindowModeText;
+    public Slider MasterVolSlider;
+    public Slider MusicVolSlider;
+    public Slider EffectVolSlider;
+
+
+    public SettingsInfo SettingInfo;
+
+    private MenuNavigator _MenuNavigator;
+
+    private xb_gamepad gamepad;
+
+    private MenuNavigator.MenuArea SettingsMenu;
+
+    // Use this for initialization
+    void Start () {
+        gamepad = GamepadManager.Instance.GetGamepad(1);
+        _MenuNavigator = GetComponent<MenuNavigator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavigateSettingsMenu();
+        SwitchSettings();
+        UpdateText();
+        Debug.Log(CurrentSelection);
+    }
+
+
+    /// <summary>
+    /// Saves player settings to player prefs.
+    /// </summary>
+    public void SavePlayerSettings()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
+        PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
+        PlayerPrefs.SetFloat("SFXVolume", EffectsVolume);
+        PlayerPrefs.SetInt("Resolution", (int)_Resolution);
+        PlayerPrefs.SetInt("AspectRatio", (int)_AspectRatio);
+        PlayerPrefs.SetInt("VSync", VSync);
+        PlayerPrefs.SetInt("WindowMode", (int)_WindowMode);
+        PlayerPrefs.SetInt("TextureQuality", (int)_TextureQuality);
+        PlayerPrefs.Save();
+        Debug.Log("Player Settings Saved!");
+    }
+
+    
+    private void SwitchSettings()
+    {
+         switch(CurrentSelection)
+        {
+            case "Gamepad":
+                
+                break;
+            case "KeyboardBindings":
+                break;
+            case "MenuType":
+                if(gamepad.GetButtonDown("RB"))
+                {
+                    if(_MenuType < MenuType.WINDOW)
+                    _MenuType++;
+                    else
+                    {
+                        _MenuType = 0;
+                    }
+                }
+                else if(gamepad.GetButtonDown("LB"))
+                {
+                    if (_MenuType > MenuType.RADIAL)
+                        _MenuType--;
+                    else
+                    {
+                        _MenuType = MenuType.WINDOW; ;
+                    }
+                }
+                break;
+            case "WindowMode":
+                if (gamepad.GetButtonDown("RB"))
+                {
+                    if (_WindowMode < WindowMode.WINDOWED)
+                        _WindowMode++;
+                    else
+                    {
+                        _WindowMode = 0;
+                    }
+                }
+                else if (gamepad.GetButtonDown("LB"))
+                {
+                    if (_WindowMode > WindowMode.FULLSCREEN)
+                        _WindowMode--;
+                    else
+                    {
+                        _WindowMode = WindowMode.WINDOWED;
+                    }
+                }
+                break;
+            case "Resolution":
+                break;
+            case "AspectRatio":
+                break;
+            case "VSync":
+                if (gamepad.GetButtonDown("RB"))
+                {
+                   if(VSync == 0)
+                    {
+                        VSync = 1;
+                    }
+                   else if(VSync == 1)
+                    {
+                        VSync = 0;
+                    }
+                }
+                else if (gamepad.GetButtonDown("LB"))
+                {
+                    if (VSync == 0)
+                    {
+                        VSync = 1;
+                    }
+                    else if (VSync == 1)
+                    {
+                        VSync = 0;
+                    }
+                }
+                break;
+            case "TextureQuality":
+                if (gamepad.GetButtonDown("RB"))
+                {
+                    if (_TextureQuality < TextureQuality.ULTRA)
+                        _TextureQuality++;
+                    else
+                    {
+                        _TextureQuality = 0;
+                    }
+                }
+                else if (gamepad.GetButtonDown("LB"))
+                {
+                    if (_TextureQuality > TextureQuality.HIGH)
+                        _TextureQuality--;
+                    else
+                    {
+                        _TextureQuality = TextureQuality.ULTRA ;
+                    }
+                }
+                break;
+            case "MasterVolume":
+                if(gamepad.GetButton("LB"))
+                {
+                    if(MasterVolume > 0)
+                    {
+                        MasterVolume--;
+                    }
+                }
+                else if(gamepad.GetButton("RB"))
+                {
+                    if (MasterVolume < 100)
+                    {
+                        MasterVolume++;
+                    }
+                }
+                break;
+            case "MusicVolume":
+                if (gamepad.GetButton("LB"))
+                {
+                    if (MusicVolume > 0)
+                    {
+                        MusicVolume--;
+                    }
+                }
+                else if (gamepad.GetButton("RB"))
+                {
+                    if (MusicVolume < 100)
+                    {
+                        MusicVolume++;
+                    }
+                }
+                break;
+            case "SFXVolume":
+                if (gamepad.GetButton("LB"))
+                {
+                    if (EffectsVolume > 0)
+                    {
+                        EffectsVolume--;
+                    }
+                }
+                else if (gamepad.GetButton("RB"))
+                {
+                    if (EffectsVolume < 100)
+                    {
+                        EffectsVolume++;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateText()
+    {
+        if(VSync == 0)
+        {
+            VSyncText.text = "OFF";
+        }
+        else if(VSync == 1)
+        {
+            VSyncText.text = "ON";
+        }
+
+        switch(_MenuType)
+        {
+            case MenuType.RADIAL:
+                MenuTypeText.text = "RADIAL";
+                break;
+            case MenuType.WINDOW:
+                MenuTypeText.text = "WINDOW";
+                break;
+        }
+
+        switch (_TextureQuality)
+        {
+            case TextureQuality.HIGH:
+                TextureQualityText.text = "HIGH";
+                break;
+            case TextureQuality.MEDIUM:
+                TextureQualityText.text = "MEDIUM";
+                break;
+            case TextureQuality.LOW:
+                TextureQualityText.text = "LOW";
+                break;
+            case TextureQuality.ULTRA:
+                TextureQualityText.text = "ULTRA";
+                break;
+            default:
+                break;
+        }
+        
+        switch(_WindowMode)
+        {
+            case WindowMode.FULLSCREEN:
+                WindowModeText.text = "FULLSCREEN";
+                break;
+            case WindowMode.WINDOWED:
+                WindowModeText.text = "WINDOWED";
+                break;
+            default:
+                break;
+        }
+
+        switch(_Resolution)
+        {
+            case Resolution.NINETEEN_TWENTY_BY_TEN_EIGHTY:
+                ResolutionText.text = "1920 x 1080";
+                break;
+            default:
+                break;
+        }
+
+        if (_MenuNavigator.SettingsMenu.AreaState == MenuNavigator.SceneAreaState.ACTIVE)
+        {
+            if (gamepad.IsConnected)
+            {
+                SettingInfo.SelectionMenuLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.SelectionMenuRight.GetComponentInChildren<Text>().text = "RT";
+                SettingInfo.TextureQualityLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.TextureQualityRight.GetComponentInChildren<Text>().text = "RT";
+                SettingInfo.V_SyncLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.V_SyncRight.GetComponentInChildren<Text>().text = "RT";
+                SettingInfo.WindowModeLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.WindowModeRight.GetComponentInChildren<Text>().text = "RT";
+                SettingInfo.ResolutionLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.ResolutionRight.GetComponentInChildren<Text>().text = "RT";
+                SettingInfo.AspectRatioLeft.GetComponentInChildren<Text>().text = "LT";
+                SettingInfo.AspectRatioRight.GetComponentInChildren<Text>().text = "RT";
+            }
+            else
+            {
+                SettingInfo.SelectionMenuLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.SelectionMenuRight.GetComponentInChildren<Text>().text = ">";
+                SettingInfo.TextureQualityLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.TextureQualityRight.GetComponentInChildren<Text>().text = ">";
+                SettingInfo.V_SyncLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.V_SyncRight.GetComponentInChildren<Text>().text = ">";
+                SettingInfo.WindowModeLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.WindowModeRight.GetComponentInChildren<Text>().text = ">";
+                SettingInfo.ResolutionLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.ResolutionRight.GetComponentInChildren<Text>().text = ">";
+                SettingInfo.AspectRatioLeft.GetComponentInChildren<Text>().text = "<";
+                SettingInfo.AspectRatioRight.GetComponentInChildren<Text>().text = ">";
+            }
+        }
+    }
+
+    private void NavigateSettingsMenu()
+    {
+        MasterVolSlider.value = MasterVolume;
+        MusicVolSlider.value = MusicVolume;
+        EffectVolSlider.value = EffectsVolume;
+        MasterVolSlider.GetComponentInChildren<Text>().text = MasterVolume.ToString();
+        MusicVolSlider.GetComponentInChildren<Text>().text = MusicVolume.ToString();
+        EffectVolSlider.GetComponentInChildren<Text>().text = EffectsVolume.ToString();
+    }
+}
