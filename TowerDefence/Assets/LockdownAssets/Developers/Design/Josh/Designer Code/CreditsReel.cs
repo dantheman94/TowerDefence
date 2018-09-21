e8a85c988e706a7d87887caa5a3c77aeefe6b7ad
Josh (Mr Designer Man) <joshua.dagostino@outlook.com>
2018-09-22 03:53:38 +1000

    --- Updates --- === Started UI pass === - Added UI Pass scenes in Developers > Designers > Josh > _Scenes - Added credits    - Credits move up over time and can be controlled via multiplier. - Fixed up showcase scene, FOV is consistant.    - Large assets now correctly scale down on pointer enter to set value and revert back to 1,1,1 on pointer exit - Added temp representations of infantry units. (Can remove if needed, was used for testing)

---
+++ TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/CreditsReel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsReel : MonoBehaviour {
+    [Tooltip("The Image of the credits")]
+    public GameObject creditsImage;
+
+    [Tooltip("Multiply the speed of the reel by this much")]
+    public float speed;
+    
+	void Update () {
+
+        // Move Credits object up in world space by (Deltatime x speed) that is set in inspector
+        creditsImage.transform.Translate(0, Time.deltaTime*10*speed, 0, Space.World);
+    }
+}
---
+++ TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/ShowcaseScaleObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseScaleObject : MonoBehaviour {
+
+    
+    [Tooltip ("The X,Y,Z Scale to set for the Asset")]
+    public float scaleSize;
+
+    public void ScaleObject() {
+        // Sets the local scale of the object by what the scaleSize is set to.
+        transform.localScale = new Vector3(scaleSize, scaleSize, scaleSize);
+
+    }
+
+    public void ResetScale()
+    {
+        // Return the scale back to 1,1,1
+        transform.localScale = new Vector3(1, 1, 1);
+
+    }
+}
