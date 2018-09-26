aab40a5d2161a094a9ebec2e81a08e401a49844c
Josh (Mr Designer Man) <joshua.dagostino@outlook.com>
2018-09-27 06:22:04 +1000

    --- Updates --- - Fixed up Designer Scripts
    === Nav Mesh ===
    - Optimised nav mesh assets
       - Structured them in hierarchy
       - Removed unnessacery nav mesh carvings
       - Increased size and/or changed shape to better cover area to carve
    
    - Moved flying navmesh to better position. Now not below high elevation
    - Created max terrain height nav mesh carve
    
    === UI ===
    - Loading Screen UI pass 1 done
       - Created temp icons positions for Difficulty and game type
       - Rearranged text positions, better fits screen.
       - Made map overview large, moved to right side of screen
    
    - Master Scene pass 1 ~75% done
       - Increased Squad icon sizes
       - Removed button number on Squad icons. (Can't work with controller, now uniform)
       - Minimap size increased and moved
       - Units, resources and icons moved and increased in size.

--- TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/CreditsReel.cs
+++ TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/CreditsReel.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//******************************
+//
+//  Created by: Joshua D'Agostino
+//
+//  Last edited by: Joshua D'Agostino
+//  Last edited on: 27/09/2018
+//
+//******************************
 public class CreditsReel : MonoBehaviour {
+
+    //******************************************************************************************************************************
+    //
+    //      INSPECTOR
+    //
+    //******************************************************************************************************************************
+
     [Tooltip("The Image of the credits")]
     public GameObject creditsImage;
 
@@ -11,7 +25,9 @@ public class CreditsReel : MonoBehaviour {
     
 	void Update () {
 
-        // Move Credits object up in world space by (Deltatime x speed) that is set in inspector
+        /// <summary>
+        //  Move Credits object up in world space by (Deltatime x speed) that is set in inspector
+        /// </summary> 
         creditsImage.transform.Translate(0, Time.deltaTime*10*speed, 0, Space.World);
     }
 }
--- TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/ShowcaseScaleObject.cs
+++ TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/ShowcaseScaleObject.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//******************************
+//
+//  Created by: Joshua D'Agostino
+//
+//  Last edited by: Joshua D'Agostino
+//  Last edited on: 27/09/2018
+//
+//******************************
 public class ShowcaseScaleObject : MonoBehaviour {
 
-    
+    //******************************************************************************************************************************
+    //
+    //      INSPECTOR
+    //
+    //******************************************************************************************************************************
+
     [Tooltip ("The X,Y,Z Scale to set for the Asset")]
     public float scaleSize;
 
+//******************************************************************************************************************************
+    //
+    //      FUNCTIONS
+    //
+    //******************************************************************************************************************************
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+    
+    /// <summary>
+    //  Called when pointer enters button. Use with Event Trigger
+    /// </summary>
     public void ScaleObject() {
         // Sets the local scale of the object by what the scaleSize is set to.
         transform.localScale = new Vector3(scaleSize, scaleSize, scaleSize);
-
     }
 
+
+    /// <summary>
+    //  Called when pointer exits button. Use with Event Trigger
+    /// </summary>
     public void ResetScale()
     {
         // Return the scale back to 1,1,1
