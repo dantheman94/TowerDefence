0b8246171fc1fdf2093f094215db8648d3f2b4f0
AIE\s170998 <eljosho1998@gmail.com>
2018-10-01 12:27:05 +1000

    Resource Manager script update, Player and Generator scripts updated.

--- TowerDefence/Assets/LockdownAssets/Developers/Programming/Josh/ResourceManager.cs
+++ TowerDefence/Assets/LockdownAssets/Developers/Programming/Josh/ResourceManager.cs
@@ -19,22 +19,33 @@ public class ResourceManager : MonoBehaviour {
     //
     //******************************************************************************************************************************
 
-    public Player _Player;
-    public Resources _Resources;
-    public ResourceNode _ResourceNode;
+    private Player _Player;
 
-    public int _ResourceGenerators;
-    public int _ResourceType;
+    [Space]
+    [Header("-----------------------------------")]
+    [Header(" GENERATOR COUNT")]
+    [Space]
+    private int _SupplyGenerators;
+    private int _PowerGenerators;
 
-    public float _ResourceGenerationAmount;
-    public float _ResourceGenerationRate;
-    public float _ResourceGenerationMultiplier;
+    [Space]
+    [Header("-----------------------------------")]
+    [Header(" TIMERS")]
+    [Space]
+    private float _SupplyTimer = 0f;
+    private float _PowerTimer = 0f;
 
     //******************************************************************************************************************************
     //
     //      INSPECTOR
     //
     //******************************************************************************************************************************
+    [Space]
+    [Header("-----------------------------------")]
+    [Header(" GENERATOR OBJECTS")]
+    [Space]
+    public Generator SupplyGenerator;
+    public Generator PowerStation;
 
     //******************************************************************************************************************************
     //
@@ -44,15 +55,63 @@ public class ResourceManager : MonoBehaviour {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /// <summary>
+    ///  Called when the gameObject is created.
+    /// </summary>
     void Start () {
-		
+        // Get Player component
+        _Player = GetComponent<Player>();	
 	}
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    void Update () {
-		
-	}
+    /// <summary>
+    ///  Called each frame.
+    /// </summary>
+    void Update()
+    {
+        if (_Player != null)
+        {
+
+            // Keep generating resources for the player
+            if (_SupplyTimer < SupplyGenerator.GeneratorRate / _SupplyGenerators) {
+
+                _SupplyTimer += Time.deltaTime;
+            }
+            else {
+
+                if ((_Player.SuppliesCount < _Player.MaxSupplyCount) && _SupplyGenerators >= 1) {
+
+                    _Player.SuppliesCount += SupplyGenerator.ResourcesGivenPerTickOver;
+                }
+                // Reset timer
+                _SupplyTimer = 0f;
+            }
+
+            // Keep generating power for the player
+            if (_PowerTimer < PowerStation.GeneratorRate / _PowerGenerators) {
+
+                _PowerTimer += Time.deltaTime;
+            }
+            else {
+
+                if ((_Player.PowerCount < _Player.MaxSupplyCount) && _PowerGenerators >= 1) {
+
+                    _Player.PowerCount += PowerStation.ResourcesGivenPerTickOver;
+                }
+                // Reset timer
+                _PowerTimer = 0f;
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void AddSupplyGeneratorCount() { _SupplyGenerators += 1; }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void AddPowerGeneratorCount() { _PowerGenerators += 1; }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
