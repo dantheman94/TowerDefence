9cfd5b584ce6c5469ae2300f80fe85294bb77341
AIE\s170998 <eljosho1998@gmail.com>
2018-10-09 12:24:39 +1100

    SoundManager updated to play music.

--- TowerDefence/Assets/LockdownAssets/Scripts/Singletons/SoundManager.cs
+++ TowerDefence/Assets/LockdownAssets/Scripts/Singletons/SoundManager.cs
@@ -29,17 +29,21 @@ public class SoundManager : MonoBehaviour {
 
     private bool _FadingIn;
     private bool _FadingOut;
-
     private float _CurrentFadeInLerp = 0f;
     private float _CurrentFadeOutLerp = 0f;
+
+    private const int _TrackCount = 4;
+    private bool[]  _TracksPlayed = new bool[_TrackCount] { false, false, false, false };
+    private string[] _TrackPath = new string[_TrackCount] { "Audio/Denny_Schneidemesser_-_Morning Adventure",
+                                                            "Audio/Denny_Schneidemesser_-_Entering_The_Stronghold",
+                                                            "Audio/Denny_Schneidemesser_-_The_Revolution",
+                                                            "Audio/Denny_Schneidemesser_-_With_gun_and_Crucifix" };
 
     //******************************************************************************************************************************
     //
     //      INSPECTOR
     //
     //******************************************************************************************************************************
-
-    public AudioSource _MusicSource;
 
     //******************************************************************************************************************************
     //
@@ -65,6 +69,12 @@ public class SoundManager : MonoBehaviour {
 
         // Initialize lists
         _Sounds = new List<AudioSource>();
+
+    }
+
+    private void Start() {
+        
+        PlayFirstTrack(_TrackPath[0]);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,7 +105,6 @@ public class SoundManager : MonoBehaviour {
         
         // Create pooled game object for the sound
         GameObject soundObj = ObjectPooling.Spawn(Resources.Load<GameObject>(soundLocation));
-
         // Grab the source for the sound to play from
         AudioSource soundSource = soundObj.GetComponent<AudioSource>();
 
@@ -113,6 +122,102 @@ public class SoundManager : MonoBehaviour {
 
         // Add the sound object to the List
         _Sounds.Add(soundSource);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Play the first music track.
+    /// </summary>
+    public void PlayFirstTrack(string musicLocation) {
+
+        // Create pooled game object for the music
+        GameObject musicObj = ObjectPooling.Spawn(Resources.Load<GameObject>(musicLocation));
+        // Grab the source for the music to play from
+        AudioSource musicSource = musicObj.GetComponent<AudioSource>();
+
+        // Play the music
+        musicSource.Play();
+        // Set track to true to indicate its been played
+        _TracksPlayed[0] = true;
+
+        // Start coroutine
+        StartCoroutine(UpdateTracks(musicSource));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Play the second to fourth music tracks.
+    /// </summary>
+    public void PlayTrack() {
+
+        // Reset song played counter
+        int playedCount = 0;
+
+        // Resets all the tracks to playable again if all the tracks have been played
+        for (int i = 0; i < _TracksPlayed.Length - 1; i++) {
+
+            if (_TracksPlayed[i] == true) { playedCount++; }
+        }
+
+        // Reset the tracks to false so they can be played again
+        if (playedCount == _TrackCount) {
+
+            for (int i = 0; i < _TracksPlayed.Length - 1; i++) {
+
+                _TracksPlayed[i] = false;
+            }
+
+            // Set the first track to true so it only ever gets played once
+            _TracksPlayed[0] = true;
+        }
+
+        // Set to false to find a new track to play
+        bool foundTrack = false;
+
+        // Searches for a new track to play
+        while(foundTrack) {
+
+            // Generate random number
+            int i = Random.Range(1, _TrackCount);
+
+            // If the track found hasn't been played
+            if (_TracksPlayed[i] == false) {
+
+                // Create pooled game object for the music
+                GameObject musicObj = ObjectPooling.Spawn(Resources.Load<GameObject>(_TrackPath[i]));
+                // Grab the source for the music to play from
+                AudioSource musicSource = musicObj.GetComponent<AudioSource>();
+
+                // Play the music
+                musicSource.Play();
+
+                // Set to true
+                foundTrack = true;
+
+                // Set track to true to indicate its been played
+                _TracksPlayed[i] = true;
+
+                // Start coroutine
+                StartCoroutine(UpdateTracks(musicSource));
+                break;
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Update the music playing.
+    /// </summary>
+    public IEnumerator UpdateTracks(AudioSource track)
+    {
+        // Check if the current music playing is finished
+        yield return new WaitUntil(() => !track.isPlaying);
+
+        // If so, play the next
+        PlayTrack();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
