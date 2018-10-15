13ec5964a601bdc90bd2701765cf7fc163c9792d
AIE\s167697 <dmarton94@gmail.com>
2018-10-15 16:51:55 +1100

    - Updated terrain (more rock cliffs) - Added the core zapper ability to the townhall prefabs

---
+++ TowerDefence/Assets/LockdownAssets/Scripts/Selectables/Abstracts/Abilities/Ability_CoreZapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//******************************
+//
+//  Created by: Daniel Marton
+//
+//  Last edited by: Daniel Marton
+//  Last edited on: 15/10/2018
+//
+//******************************
+
+public class Ability_CoreZapper : Abstraction {
+    
+}
