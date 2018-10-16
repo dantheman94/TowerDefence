04409faf3df08b567947f0e19ca5c47982ba97cb
Josh (Mr Designer Man) <joshua.dagostino@outlook.com>
2018-10-16 23:59:32 +1100

    --- Updates --- - Made debug objects off by default - Made a script that toggles the list of them on and off when a assigned key is pressed. - Added the script to the debugger prefab.

---
+++ TowerDefence/Assets/LockdownAssets/Developers/Design/Josh/Designer Code/DebugToggle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//******************************
+//
+//  Created by: Joshua D'Agostino
+//
+//  Last edited by: Joshua D'Agostino
+//  Last edited on: 16/10/2018
+//
+//******************************
+
+public class DebugToggle : MonoBehaviour {
+
+    //******************************************************************************************************************************
+    //
+    //      INSPECTOR
+    //
+    //******************************************************************************************************************************
+
+    public KeyCode keyToUse;
+
+    public List<GameObject> objectsToToggle;
+
+    private bool keyPressedToggle;
+
+    //******************************************************************************************************************************
+    //
+    //      FUNCTIONS
+    //
+    //******************************************************************************************************************************
+
+    void Update()
+    {
+        DoTheToggle();
+    }
+
+
+    /// <summary>
+    /// Goes through each item in the list and enables them and disables them if the button is pressed again
+    /// </summary>
+    void DoTheToggle()
+    {
+        if (Input.GetKeyDown(keyToUse))
+        {
+            
+            keyPressedToggle = !keyPressedToggle; //Toggles bool each time key is pressed
+
+            if (keyPressedToggle)//Enable the objects
+            {
+                foreach (GameObject obj in objectsToToggle)
+                {
+                    obj.SetActive(true);
+                }
+            }
+            else { //Disables the objects
+                foreach (GameObject obj in objectsToToggle)
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
