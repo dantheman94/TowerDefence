a08ed68acf095dee9d99029711eef5c115b62d83
AIE\s170998 <eljosho1998@gmail.com>
2018-10-16 13:51:16 +1100

    Particle script update.

--- TowerDefence/Assets/LockdownAssets/Developers/Programming/Josh/Particles.cs
+++ TowerDefence/Assets/LockdownAssets/Developers/Programming/Josh/Particles.cs
@@ -45,60 +45,79 @@ public class Particles : MonoBehaviour {
 
         _Renderer = GetComponent<Renderer>();
         _Renderer.enabled = true;
-        _Player = GameManager.Instance.Players[0];
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    void Update()
-    {
+    void Awake() {
 
-        // Calculate distance from the current position to the screen center
-        _DistanceFromScreenCenter = Vector3.Distance(transform.position, _ScreenCenter);
+    }
 
-        // Get player camera position and store it into a Vector3
-        Vector3 camPos = _Player.CameraAttached.WorldToScreenPoint(transform.position);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // If the player is using a keyboard
-        if (_Player._KeyboardInputManager.IsPrimaryController) {
+    void Update() {
 
-            // If the particle object is within the rect
-            if (Player.SelectionScreen.Contains(camPos)) {
+        if (_Player == null) {
 
-                // Set to true
-                _IsInScreenSpace = true;
-                _Renderer.enabled = true;
-            }
-
-            // If the particle object is not within the rect
-            else {
-
-                // Set to false
-                _IsInScreenSpace = false;
-                _Renderer.enabled = false;
-            }
+            _Player = GameManager.Instance.Players[0];
         }
 
-        // If the player is using a gamepad
-        if (_Player._XboxGamepadInputManager.IsPrimaryController)
-        {
+        else {
 
-            // If the particle object is within the rect
-            if (Player.SelectionScreen.Contains(camPos))
-            {
+            // Calculate distance from the current position to the screen center
+            _DistanceFromScreenCenter = Vector3.Distance(transform.position, _ScreenCenter);
 
-                // Set to true
-                _IsInScreenSpace = true;
-                _Renderer.enabled = true;
+            // Get player camera position and store it into a Vector3
+            Vector3 camPos = _Player.CameraAttached.WorldToScreenPoint(transform.position);
+
+            // If the player is using a keyboard
+            if (_Player._KeyboardInputManager.IsPrimaryController) {
+
+                // If the particle object is within the rect
+                if (Player.SelectionScreen.Contains(camPos)) {
+
+                    // Set to true
+                    _IsInScreenSpace = true;
+                    _Renderer.enabled = true;
+                    // Set to false
+                    gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+                    gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Play();
+
+                    Debug.Log("Object is within the camera view.");
+                }
+
+                // If the particle object is not within the rect
+                else {
+
+                    // Set to false
+                    _IsInScreenSpace = false;
+                    _Renderer.enabled = false;
+                    // Set to true
+                    gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Pause();
+                    gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Pause();
+
+                    Debug.Log("Object is not within the camera view.");
+                }
             }
 
-            // If the particle object is not within the rect
-            else
-            {
+            // If the player is using a gamepad
+            if (_Player._XboxGamepadInputManager.IsPrimaryController) {
 
-                // Set to false
-                _IsInScreenSpace = false;
-                _Renderer.enabled = false;
+                // If the particle object is within the rect
+                if (Player.SelectionScreen.Contains(camPos)) {
+
+                    // Set to true
+                    _IsInScreenSpace = true;
+                    _Renderer.enabled = true;
+                }
+
+                // If the particle object is not within the rect
+                else {
+
+                    // Set to false
+                    _IsInScreenSpace = false;
+                    _Renderer.enabled = false;
+                }
             }
         }
     }
