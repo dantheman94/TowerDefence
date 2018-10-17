52f5532fe18e6b782de3e72a953abf6b68a646cc
DANS-PC\Daniel <dmarton94@gmail.com>
2018-10-17 15:05:51 +1100

    - Setup the abilities for easier integration

--- TowerDefence/Assets/LockdownAssets/Scripts/Selectables/Abstracts/Abilities/Ability_Veterancy.cs
+++ TowerDefence/Assets/LockdownAssets/Scripts/Selectables/Abstracts/Abilities/Ability_Veterancy.cs
@@ -7,10 +7,36 @@ using UnityEngine;
 //  Created by: Daniel Marton
 //
 //  Last edited by: Daniel Marton
-//  Last edited on: 15/10/2018
+//  Last edited on: 17/10/2018
 //
 //******************************
 
 public class Ability_Veterancy : Abstraction {
-    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    //  Called when the player presses a button on the selection wheel with this world object linked to the button.
+    /// </summary>
+    /// <param name="buildingSlot">
+    //  The building slot that instigated the selection wheel.
+    //  (EG: If you're making a building, this is the building slot thats being used.)
+    /// </param>
+    public override void OnWheelSelect(BuildingSlot buildingSlot) {
+
+        // Deselect the building slot
+        buildingSlot.SetIsSelected(false);
+
+        // Get player reference
+        Player plyr = GameManager.Instance.Players[0];
+
+        // Check if the player has enough resources to build the object
+        if ((plyr.SuppliesCount >= this.CostSupplies) && (plyr.PowerCount >= this.CostPower)) {
+
+            // DO SOME SHIT BRO
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    }
 }
