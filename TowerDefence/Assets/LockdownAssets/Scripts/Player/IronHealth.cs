ebcf3949d62208e31b6d2f2344bba9ea829ab0bb
Angus <angycs123@hotmail.com>
2018-10-22 10:15:03 +1100

    Added iron health ability. added get building list to the base list.

---
+++ TowerDefence/Assets/LockdownAssets/Scripts/Player/IronHealth.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-=-=-=-=-=-=-=-=-=-=-=-=
+//Created by: Angus Secomb
+//Last modified: 22/10/18
+//Editor: Angus
+//-=-=-=-=-=-=-=-=-=-=-=-=
+
+public class IronHealth : MonoBehaviour {
+
+     
+    //         INSPECTOR
+    /////////////////////////////////////////////
+
+    public float HealthTimer = 15.0f;
+
+
+
+    //        VARIABLES
+    //////////////////////////////////////////////
+
+    private Base _DesiredBase;
+    private List<Building> _Buildings;
+    private bool _OnDestroy = false;
+
+
+
+    //        FUNCTIONS
+    //////////////////////////////////////////////
+
+    // Update is called once per frame
+    void Update () {
+        
+        HealthTimer -= Time.deltaTime;
+        if(HealthTimer < 0)
+        {
+            if(!_OnDestroy)
+            {
+                DestroyObject();
+            }
+
+        }
+	}
+
+    /// <summary>
+    /// Gets list of buildings from the base.
+    /// </summary>
+    /// <param name="buildings"></param>
+    public void SetBuildings(List<Building> buildings) { _Buildings = buildings; }
+
+    /// <summary>
+    /// Gets base reference.
+    /// </summary>
+    /// <param name="a_base"></param>
+    public void SetBase(Base a_base) { _DesiredBase = a_base; }
+
+    /// <summary>
+    /// Destroys the iron health object.
+    /// </summary>
+    void DestroyObject()
+    {
+        _OnDestroy = true;
+        _DesiredBase.SetCanBeDamaged(true);
+        for(int i =0; i < _Buildings.Count; ++i)
+        {
+            _Buildings[i].SetCanBeDamaged(true);
+        }
+      
+        Destroy(this.gameObject);
+    }
+}
--- TowerDefence/Assets/LockdownAssets/Scripts/Selectables/Abstracts/Abilities/Ability_BaseHealth.cs
+++ TowerDefence/Assets/LockdownAssets/Scripts/Selectables/Abstracts/Abilities/Ability_BaseHealth.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 //
 //  Created by: Daniel Marton
 //
-//  Last edited by: Daniel Marton
-//  Last edited on: 17/10/2018
+//  Last edited by: Angus Secomb
+//  Last edited on: 22/10/2018
 //
 //******************************
 
 public class Ability_BaseHealth : Abstraction {
+
+    private GameObject _ReferenceObject;
+    private IronHealth _IronHealth;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -23,16 +26,30 @@ public class Ability_BaseHealth : Abstraction {
     //  (EG: If you're making a building, this is the building slot thats being used.)
     /// </param>
     public override void OnWheelSelect(BuildingSlot buildingSlot) {
-
+        
+        _IronHealth = new IronHealth();
+        _ReferenceObject = new GameObject();
         // Deselect the building slot
         buildingSlot.SetIsSelected(false);
 
         // Get player reference
         Player plyr = GameManager.Instance.Players[0];
+     
+        //Set references for the instantiated object.
+        _IronHealth = _ReferenceObject.AddComponent(typeof(IronHealth)) as IronHealth;
+        _IronHealth.SetBase(buildingSlot.AttachedBase);
+        _IronHealth.SetBuildings(buildingSlot.AttachedBase.GetBuildingList());
 
         // Check if the player has enough resources to build the object
         if ((plyr.SuppliesCount >= this.CostSupplies) && (plyr.PowerCount >= this.CostPower)) {
 
+            buildingSlot.AttachedBase.SetCanBeDamaged(false);
+            for(int i = 0; i < buildingSlot.AttachedBase.GetBuildingList().Count; ++i)
+            {
+                buildingSlot.AttachedBase.GetBuildingList()[i].SetCanBeDamaged(false);
+            }
+            Debug.Log("Iron health activated! ");
+            
             // DO SOME SHIT BRO
         }
 
